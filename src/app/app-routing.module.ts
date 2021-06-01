ddb8f4b14e8ae9dad0b50c10885b1ef01d1c6948
nirmalp96 <184133p@uom.lk>
2021-06-01 17:37:26 +0530

    first init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TutorialComponent} from './tutorial/tutorial.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'tutorial', component:TutorialComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatSliderModule } from '@angular/material/slider';
+import {  MatIconModule } from '@angular/material/icon';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { TutorialComponent } from './tutorial/tutorial.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TutorialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
